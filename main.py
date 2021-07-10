5035de4636dcd64521695e97946c05239ef893d1
Maxim <maximrez@mail.ru>
2021-07-10 11:07:50 +0300

    commit

--- main.py
+++ main.py
@@ -9,16 +9,10 @@ if __name__ == "__main__":
     test_data_dir = os.path.join(data_dir, 'test', 'videos')
     shutil.unpack_archive(os.path.join(data_dir, 'test_videos.zip'), os.path.join(data_dir, 'test'))
 
-    file_name = "corgi.mp4"
+    file_name = "dog.mp4"
     file_path = os.path.join(test_data_dir, file_name)
 
     out_name = "output.avi"
     out_path = os.path.join(data_dir, out_name)
 
     process_video(file_path, project_dir, out_path, True, False)
-
-    file_name = "input.avi"
-    file_path = os.path.join(data_dir, file_name)
-
-    out_name = "output.mp4"
-    out_path = os.path.join(data_dir, out_name)
